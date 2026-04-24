100a41afc7548dade3f12e10d9ed01cb49ae503b
MEESALA SIVA SAI AKHIL <akhilmeesala72@gmail.com>
2026-04-24 12:22:39 +0530

    Update S3 bucket and region in data.tf

--- 2-eks/data.tf
+++ 2-eks/data.tf
@@ -2,8 +2,8 @@ data "terraform_remote_state" "network" {
   backend = "s3"
 
   config = {
-    bucket = "quantamvector-infra-statefile-backup"
+    bucket = "dev973-infra-statefile-backup"
     key    = "quantamvector/1-network/terraform.tfstate"
-    region = "ap-northeast-1"
+    region = "ap-south-1"
   }
 }

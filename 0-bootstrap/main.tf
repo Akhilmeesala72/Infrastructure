d7e1461a05d6029c645b916e7b101b2a2819d7ea
MEESALA SIVA SAI AKHIL <akhilmeesala72@gmail.com>
2026-04-24 12:17:01 +0530

    Change S3 bucket name for state file backup

--- 0-bootstrap/main.tf
+++ 0-bootstrap/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "tf_state" {
-  bucket = "quantamvector-infra-statefile-backup"
+  bucket = "dev973-infra-statefile-backup"
 }
 
 resource "aws_s3_bucket_versioning" "tf_state" {
